b7daf1bb8d8258c7e8fb69c130d2ba0c402a8e1b
Yofty <106748943+Yofty@users.noreply.github.com>
2022-11-08 14:43:16 -0800

    shell sort

---
+++ 100-shell_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * shell_sort - sort array using knuth sequence
+ * @array: the array to be sorted
+ * @size: the size of the array
+ * Return: nothing
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t gap = 0, x, y;
+	int tmp;
+
+	if (size < 2)
+		return;
+
+	while ((gap = gap * 3 + 1) < size)
+		;
+
+	gap = (gapp - 1) / 3;
+
+	for (; gap > 0; gap = (gap - 1) / 3)
+	{
+		for ( x = gap; x < size; x++)
+		{
+			tmp = array[x];
+			for (y = x; y >= gap && tmp <= array[y - gap]; y -= gap)
+				array[y] = array[y - gap];
+			array[y] = tmp;
+		}
+		print_array(array, size);
+	}
+}
